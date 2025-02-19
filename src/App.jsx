d1f915c983c352d3cc231e5e53dc5bf306be32c3
Sayid-Hamza <hamzat20031996@gmail.com>
2025-02-19 14:57:06 +0300

    Install deps, create file structure based on FSD design and reset default css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import "./reset.css";
+import "./index.css";
 
 const App = () => {
-
   return (
     <>
-    <h1>hello world</h1>
+      <h1>hello world</h1>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
